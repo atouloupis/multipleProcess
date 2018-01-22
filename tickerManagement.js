480c8d8ee9ae3c5a9a5012709937b5b5f40380ca
atouloupis <atouloupis@yahoo.Fr>
2018-01-22 13:26:30 +0100

    Add files via upload

--- tickerManagement.js
+++ tickerManagement.js
@@ -30,6 +30,7 @@ jsonfile.readFile(keyfile, function(err, obj) {
                         "sKey": obj.hitbtc.sKey
                     }
                 };
+				console.log(rqstTicker);
     wsCall.webSocketCall(dbase,rqstTicker, rqstAuth);
 	
 	});
