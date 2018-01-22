ed1ab94c77c08d321f4f7cc43020c4df2400da85
atouloupis <atouloupis@yahoo.Fr>
2018-01-22 10:55:05 +0100

    Add files via upload

--- activeOrdersManagement.js
+++ activeOrdersManagement.js
@@ -22,7 +22,7 @@ mongoClient.connect(urlOrderBook, function(err, db) {
 
             jsonfile.readFile(keyfile, function(err, obj) {
                 if (err) throw err;
-				console.log(obj.hitbtc.pKey);
+				console.log(obj);
 				console.log(obj.hitbtc.sKey);
                 var rqstAuth = {
                     "method": "login",
