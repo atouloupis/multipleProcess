1ed182210d49c9c175d516b5cd3abe6eb129cd11
atouloupis <atouloupis@yahoo.Fr>
2018-01-23 10:18:06 +0100

    Add files via upload

--- launcher.js
+++ launcher.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 
-var activeOrdersManagement = require('activeOrdersManagement');
-var orderBookManagement = require('orderBookManagement');
-var symbolManagement = require('symbolManagement');
-var tradesManagement = require('tradesManagement');
+var activeOrdersManagement = require('./activeOrdersManagement');
+var orderBookManagement = require('./orderBookManagement');
+var symbolManagement = require('./symbolManagement');
+var tradesManagement = require('./tradesManagement');
