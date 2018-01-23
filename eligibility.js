296df4080bce4258ad5f8dbd08dbccb32d3fb830
atouloupis <atouloupis@yahoo.Fr>
2018-01-23 10:29:24 +0100

    Add files via upload

--- eligibility.js
+++ eligibility.js
@@ -11,8 +11,6 @@ function sell(dbase,ticker, callback) {
     //api.getHitBTC("/api/2/order?symbol=" + ticker.symbol, "delete", function(err, result) {
         //if (err) console.log("delete error" + JSON.stringify(err));
         //Récupérer le dernier trade history d'achat. A savoir combien on l'a acheté
-        console.log("sell");
-		console.log(ticker);
 		getReports.getLastBuyTrade(dbase,ticker.symbol, function(lastBuyTrade) {
             console.log("lastBuyTrade"+lastBuyTrade.price);
             // il faut vérifier combien il y a sur le compte pour cette monnaie
