b6fe2c70cfc39a8d24743e00b044950afb2bbb59
atouloupis <atouloupis@yahoo.Fr>
2018-01-24 10:40:00 +0100

    Add files via upload

--- wsCall.js
+++ wsCall.js
@@ -10,7 +10,6 @@ function webSocketCall(dbase,rqst, rqstAuth) {
 	console.log("CONNECTED");
         ws.onerror = function(evt) {};
         ws.onmessage = function(evt) {
-		console.log(evt.data);
             treatment.splitFrame(dbase,evt.data);
         };
 
@@ -22,7 +21,6 @@ function webSocketCall(dbase,rqst, rqstAuth) {
 		{
             for (var i=0;i<rqst.length;i++)
             {
-			console.log (rqst[i]);
                 sendRequest(rqstAuth, function() {
                     sendRequest(rqst[i], function () {});
 		});
