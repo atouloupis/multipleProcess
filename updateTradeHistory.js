70cf2869c8c3b18f38771548c0509097a992e5e5
atouloupis <atouloupis@yahoo.Fr>
2018-01-25 12:44:55 +0100

    Add files via upload

--- updateTradeHistory.js
+++ updateTradeHistory.js
@@ -1,19 +1,26 @@
 module.exports.newTradeHistory = newTradeHistory;
 var mongoDb = require('./mongoDb');
+var collectionName = "tradeHistory";
+var queryInsert=[];
 
-function newTradeHistory(dbase,frame) {
-    var collectionName = "tradeHistory";
+function newTradeHistory(dbase,frame,method) {
+if (method == "snapshotTrades") {
 	console.log(frame.symbol);
         for (var i = 0; i < frame.data.length; i++) {
-		var id=frame.data[i].id;
-		var price=frame.data[i].price;
-		var quantity=frame.data[i].quantity;
-		var side=frame.data[i].side;
-		var timestamp=frame.data[i].timestamp;
-		var symbol=frame.symbol;
-            var queryUpdate = {id:id, price:price, quantity:quantity, side:side, timestamp:timestamp, symbol:symbol};
-            mongoDb.updateCollection(dbase,collectionName, queryUpdate, {$set:queryUpdate}, function () {
-                mongoDb.createIndex(dbase,collectionName,"{symbol:1,timestamp:-1}",function(){});
-            });
+            queryInsert.push({id:frame.data[i].id, price:frame.data[i].price, quantity:frame.data[i].quantity, side:frame.data[i].side, timestamp:frame.data[i].timestamp, symbol:frame.symbol});
         }
+		if (i==frame.data.length){
+		console.log("insert");
+		mongoDb.insertCollection(dbase,collectionName, queryInsert, function() {
+		mongoDb.createIndex(dbase,collectionName,"{symbol:1,timestamp:-1}",function(){});
+		});
+		}
+}
+else {
+        for (var i = 0; i < frame.data.length; i++) {
+            var queryUpdate = {id:frame.data[i].id, price:frame.data[i].price, quantity:frame.data[i].quantity, side:frame.data[i].side, timestamp:frame.data[i].timestamp, symbol:frame.symbol};
+            mongoDb.updateCollection(dbase,collectionName, queryUpdate, {$set:queryUpdate}, function () {});
+        }
+	}
+	
 }
