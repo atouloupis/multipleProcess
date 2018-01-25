40b6b45293aa61ff3873523041db7be8b32ddd40
atouloupis <atouloupis@yahoo.Fr>
2018-01-25 12:31:43 +0100

    Add files via upload

--- updateTradeHistory.js
+++ updateTradeHistory.js
@@ -3,6 +3,7 @@ var mongoDb = require('./mongoDb');
 
 function newTradeHistory(dbase,frame) {
     var collectionName = "tradeHistory";
+	console.log(frame.symbol);
         for (var i = 0; i < frame.data.length; i++) {
 		var id=frame.data[i].id;
 		var price=frame.data[i].price;
@@ -10,7 +11,6 @@ function newTradeHistory(dbase,frame) {
 		var side=frame.data[i].side;
 		var timestamp=frame.data[i].timestamp;
 		var symbol=frame.symbol;
-		console.log(symbol);
             var queryUpdate = {id:id, price:price, quantity:quantity, side:side, timestamp:timestamp, symbol:symbol};
             mongoDb.updateCollection(dbase,collectionName, queryUpdate, {$set:queryUpdate}, function () {
                 mongoDb.createIndex(dbase,collectionName,"{symbol:1,timestamp:-1}",function(){});
