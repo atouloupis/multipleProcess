5c198f68f9d7768d52aa2dd5acc27afcffb871f3
atouloupis <atouloupis@yahoo.Fr>
2018-02-14 13:11:09 +0100

    Add files via upload

--- bestSymbolList.js
+++ bestSymbolList.js
@@ -5,6 +5,6 @@ var diffPerc = 0;
 for (var i=0;i<allSymbols.length;i++)
 	{
 	diffPerc=((allSymbols[i].ask/allSymbols[i].bid)-1)*100;
-	if (diffPerc >2) console.log(allSymbols[i].symbol);
+	if (diffPerc >2 && (allSymbols[i].symbol.search(#ETH#))) console.log(allSymbols[i].symbol);
 	}
 });
